f1a375112432c83e85c98d1cb8d65f8f6e43d038
Riya Singh <129310986+Riyasingh90@users.noreply.github.com>
2023-10-08 04:17:23 +0530

    Create mergesort.cpp

---
+++ mergesort.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Merge two sorted subarrays into one sorted array
+void merge(vector<int>& arr, int left, int middle, int right) {
+    int n1 = middle - left + 1; // Size of the left subarray
+    int n2 = right - middle;    // Size of the right subarray
+
+    // Create temporary arrays for the left and right subarrays
+    vector<int> leftArr(n1);
+    vector<int> rightArr(n2);
+
+    // Copy data to temporary arrays leftArr[] and rightArr[]
+    for (int i = 0; i < n1; i++) {
+        leftArr[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        rightArr[j] = arr[middle + 1 + j];
+    }
+
+    // Merge the two temporary arrays back into the original array arr[]
+    int i = 0; // Initial index of left subarray
+    int j = 0; // Initial index of right subarray
+    int k = left; // Initial index of merged subarray
+
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of leftArr[], if any
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of rightArr[], if any
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+}
+
+// Main function to perform Merge Sort on the entire array
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2; // Calculate the middle index
+
+        // Sort the first and second halves
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, middle, right);
+    }
+}
+
+int main() {
+    vector<int> arr = {12, 11, 13, 5, 6, 7};
+
+    cout << "Original array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    mergeSort(arr, 0, arr.size() - 1);
+
+    cout << "Sorted array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
